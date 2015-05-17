2e8457be049f3b79c4540ec7a785749dca7da9da
TheChronicMonster <legendzing@gmail.com>
2015-05-17 06:25:13 -0500

    Update main.js
    nearly complete commit

--- maps/js/main.js
+++ maps/js/main.js
@@ -1,3 +1,11 @@
+//TODO: Prettify with CSS
+
+//TODO: Implement a third party API that adds content to infoWindow
+
+//TODO: Implement graceful failures
+
+// If there is one thing this project has taught me it is that
+// Scope Matters
 
 var map = new google.maps.Map(document.getElementById('map-canvas'), 
 {
@@ -6,87 +14,97 @@ var map = new google.maps.Map(document.getElementById('map-canvas'),
 	disableDefaultUI: true
 });
 
-var infowindow = new google.maps.InfoWindow({ });
-
-function myViewModel() {
-	this.mapPoints = ko.observableArray([
-        new mapPoint('Citrus Cafe', 'Restaurant', 42.680,-89.019),
-        new mapPoint('Cozy Inn Chinese Restaurant', 'Restaurant', 42.681,-89.026),
-        new mapPoint('Hacienda Real Mexican Restaurant', 'Restaurant', 42.716,-88.995),
-        new mapPoint('ORiley and Conways Irish Pub', 'Bar', 42.681,-89.026),
-        new mapPoint('HHFFRRRGGH Inn', 'Bar', 42.669,-88.962),
-        new mapPoint('JPAC', 'Entertainment', 42.685,-89.013),
-        new mapPoint('Janesville Ice Arena', 'Entertainment', 42.676,-89.013),
-        new mapPoint('Wildwood Movies 16', 'Entertainment', 42.718,-88.979),
-        new mapPoint('Palmer Park', 'Park', 42.673,-88.998),
-        new mapPoint('Rotary Botanical Garden', 'Park', 42.672,-89.001),
-        new mapPoint('Riverside Park', 'Park', 42.712,-89.041),
-        ]);
-		
-	this.selectedMapPoint = ko.observable();
-
-
-	// Sets the map on all markers in the array
-	this.setMapOnMapPoints = function(map) {
-		for (var i=0; i < this.mapPoints().length; i++) {
-			this.mapPoints()[i].setMap(map);
-		}
-	}
-
-	// Remove markers from map
-	this.clearMapPoints = function() {
-		this.setMapOnMapPoints(null);
-	}
-
-	// Shows markers in array
-	this.showMapPoints= function() {
-		this.setMapOnMapPoints(map);
-		// console.log(showMarkers);
-	}
-
-	// Deletes all markers in array
-	this.deleteMapPoints= function() {
-		this.clearMapPoints();
-		this.mapPoints().removeAll();
-	}
-	
-	
-	// mapPoint object to hold information about a map point.
-	function mapPoint(name, type, lat, long) {
-		this.name = name;
-		this.lat = ko.observable(lat);
-		this.long = ko.observable(long);
-		
-		var latLng = new google.maps.LatLng(lat, long);
-
-		var marker = new google.maps.Marker({
-			position: latLng,
-			title: name,
-			map: map,
-			draggable: false
-		});
-		
-		google.maps.event.addListener(marker, 'click', function() {
-			// Create the new info window and populate with information
-			infowindow.setContent(getInfoWindowContent());
-			infowindow.open(map,marker);
-			
-			// set the center of the map to the location of the marker clicked
-			map.setCenter(latLng);
-			
-			// Update the list view to have highlight
-			this.selectedMapPoint(marker);
-		});
-		
-		function setMap(map){ 
-			marker.setMap( map );
-		};
-		
-		function getInfoWindowContent(){
-			return "Name : " + name;
-		};
-	}
-	
+// Google map fail statement
+if (typeof google != ('object')) {
+	$('#map-canvas').append("<h1>Unable to load the map. Please try again.</h1>");
 }
 
-ko.applyBindings(new myViewModel);
+var infoWindow = new google.maps.InfoWindow({ content:"stuff" });
+
+
+// mapPoint object to hold information about a map point.
+function mapPoint(name, type, lat, long, show) {
+	var self = this;
+	self.name = name;
+	self.type = type;
+	self.show = ko.observable(show);
+	self.lat = ko.observable(lat);
+	self.long = ko.observable(long);
+	self.latLng = new google.maps.LatLng(lat, long);
+
+	self.marker = new google.maps.Marker({
+		position: self.latLng,
+		title: name,
+		map: map,
+		draggable: false
+	});
+
+	self.openInfoWindow = function(){
+		// Create the new info window and populate with information
+		infoWindow.setContent(getInfoWindowContent());
+		infoWindow.open(map,self.marker);
+		
+		// set the center of the map to the location of the marker clicked
+		map.setCenter(self.latLng);
+	};
+	
+	google.maps.event.addListener(self.marker, 'click', self.openInfoWindow);
+	
+	self.updateShow = function(show){
+		self.show(show);
+		if(show){
+			self.marker.setMap( map );
+		}else{
+			self.marker.setMap( null );
+		}
+	};
+	
+	function getInfoWindowContent(){
+		var returnName = "Name : " + name;
+
+		return returnName + "<br> " + "stuff";
+		// add an ajax call from wikipedia add to content
+	}
+}	
+
+
+function myViewModel() {
+	var self = this;
+	
+	self.mapPoints = ko.observableArray([
+        new mapPoint('Citrus Cafe', 'Restaurant', 42.680,-89.019, true),
+        new mapPoint('Cozy Inn Chinese Restaurant', 'Restaurant', 42.681,-89.026, true),
+        new mapPoint('Hacienda Real Mexican Restaurant', 'Restaurant', 42.716,-88.995, true),
+        new mapPoint('ORiley and Conways Irish Pub', 'Bar', 42.681,-89.026, true),
+        new mapPoint('HHFFRRRGGH Inn', 'Bar', 42.669,-88.962, true),
+        new mapPoint('JPAC', 'Entertainment', 42.685,-89.013, true),
+        new mapPoint('Janesville Ice Arena', 'Entertainment', 42.676,-89.013, true),
+        new mapPoint('Wildwood Movies 16', 'Entertainment', 42.718,-88.979, true),
+        new mapPoint('Palmer Park', 'Park', 42.673,-88.998, true),
+        new mapPoint('Rotary Botanical Garden', 'Park', 42.672,-89.001, true),
+        new mapPoint('Riverside Park', 'Park', 42.712,-89.041, true),
+        ]);
+		
+	self.filterText = ko.observable('');
+		
+	// Selecting markers through the listview still in operation
+	self.listClick = function(mapPoint) {
+		mapPoint.openInfoWindow();
+	};
+	
+	self.filterMapPoints = ko.computed(function() {
+		var filterTextClean = self.filterText().toLowerCase();
+
+		for (var i=0; i < self.mapPoints().length; i++) {
+			if(self.mapPoints()[i].name.toLowerCase().indexOf(filterTextClean) !== -1){
+				// Show this Location
+				self.mapPoints()[i].updateShow(true);
+			}else{
+				// Hide this location
+				self.mapPoints()[i].updateShow(false);
+			}
+		}
+	}, myViewModel);
+}
+
+ko.applyBindings(new myViewModel());
